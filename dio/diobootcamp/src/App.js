3d0b125d60b7fe08507ef1a984b44fd92d421bd3
Monster243108 <evangelistanovo@gmail.com>
2022-06-15 17:07:40 -0300

    first commit

---
+++ dio/diobootcamp/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+    return (
+    <View style = {style.container}>
+        <Text>Hello Monster</Text>
+    </View>
+    );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+    container: {},
+});
